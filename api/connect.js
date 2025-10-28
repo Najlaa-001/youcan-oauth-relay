f7aca810ead1b44326d60bae57e4349c64a13566
Najlaa-001 <najlaa.sdt.bl@gmail.com>
2025-10-28 21:01:20 +0100

    back to connect ok

--- api/connect.js
+++ api/connect.js
@@ -1,3 +1,11 @@
-export default function handler(req, res) {
-  res.status(200).send("OK");
+export default async function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.YOUCAN_CLIENT_ID,
+    redirect_uri: process.env.YOUCAN_REDIRECT_URI,
+    response_type: 'code',
+    scope: 'read-orders read-customers'
+  });
+
+  const url = `https://seller-area.youcan.shop/admin/oauth/authorize?${params}`;
+  res.redirect(url);
 }

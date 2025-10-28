ff78d74a4bfc305eff6881fd7e8442b5fbfb7e6e
Najlaa-001 <najlaa.sdt.bl@gmail.com>
2025-10-28 20:43:20 +0100

    Update connect.js

--- api/connect.js
+++ api/connect.js
@@ -1,12 +1,3 @@
-export default async function handler(req, res) {
-  const params = new URLSearchParams({
-    client_id: process.env.YOUCAN_CLIENT_ID,
-    redirect_uri: process.env.YOUCAN_REDIRECT_URI,
-    response_type: 'code',
-    scope: 'read-orders read-customers',
-    state: 'secure_random_state' // optional but recommended
-  });
-
-  const url = `https://seller-area.youcan.shop/admin/oauth/authorize?${params.toString()}`;
-  return res.redirect(url);
+export default function handler(req, res) {
+  res.status(200).send("OK");
 }
